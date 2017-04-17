f26bc1deebce2161f21c04c7b9458699a30dee07
Popey Gilbert <popey@debenclipper.com>
2017-04-17 21:01:06 +0100

    Enable Menu Buttons,  Fixes #3

--- app/modules/setup.js
+++ app/modules/setup.js
@@ -2,6 +2,7 @@ const jetpack = require('fs-jetpack')
 const Datastore = require('nedb')
 const Promise = require('bluebird')
 const crypto = require('crypto')
+const { remote } = require('electron')
 
 /**
  * Sets and saves a config value to the config file.
@@ -55,6 +56,12 @@ function setup() {
 
   $(() => {
     $('#content').html(appDir.read(`./app/header.html`))
+    document.getElementById('min-btn').addEventListener('click', function(e) { remote.BrowserWindow.getFocusedWindow().minimize() })
+    document.getElementById('max-btn').addEventListener('click', function(e) { 
+      let remoteWindow = remote.BrowserWindow.getFocusedWindow()
+      remoteWindow.isMaximized() ? remoteWindow.unmaximize() : remoteWindow.maximize() 
+    })
+    document.getElementById('close-btn').addEventListener('click', function(e) { remote.BrowserWindow.getFocusedWindow().close()  })
   })
 
   logger.log(`Application Paths Found:
