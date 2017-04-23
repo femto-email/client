edd104a02306a199037e9764e8fa83610fcd276f
Popey Gilbert <popey@debenclipper.com>
2017-04-23 23:05:48 +0100

    Add compose_out.html in order to contain compose text

--- app/modules/compose.js
+++ app/modules/compose.js
@@ -1,12 +1,19 @@
 const $ = require('jquery')
 const jetpack = require('fs-jetpack')
-
+const Datastore = require('nedb')
 const { remote } = require('electron')
+global.Promise = require('bluebird')
 
 global.app = remote.app
 global.appDir = jetpack.cwd(app.getAppPath())
 global.storeDir = jetpack.cwd(app.getPath('userData'))
 global.state = storeDir.read('./state.json', 'json') || { state: 'new' }
+global.accounts = new Datastore({
+  filename: app.getPath('userData') + '/db/accounts.db',
+  autoload: true
+})
+global.accounts = Promise.promisifyAll(accounts)
+
 
 $('html')[0].innerHTML = appDir.read(`./app/main.html`)
 
@@ -23,4 +30,17 @@ $('#title').html(`
   <a href="#!" class="breadcrumb">${state.account.user}</a>
   <a href="#!" class="breadcrumb">Compose</a>
 `)
+
+$('#content').html(appDir.read(`./app/compose_out.html`))
+
+;(async () => {
+  let accountList = await accounts.findAsync({})
+  for (let i = 0; i < accountList.length; i++) {
+    $('#from').append($('<option>', {
+      value: accountList[i]._id,
+      text: accountList[i].user
+    }));
+    console.log(accountList[i])
+  }
+})()
 
